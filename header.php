d58b6a02a82149fcc6ff5d318793530f9bd29ee3
Gwendoline <schoolcheyeza@gmail.com>
2026-05-28 03:36:16 +0200

    footer and header files updated with content

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student Portal</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+<body>
+        <header>
+            <h1>UniGwen Student Portal</h1>
+            <nav>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="profile.php">Profile</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                </ul>
+            </nav>
+       
+</body>
+</html>
